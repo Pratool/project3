a1d3074719bdd5b35d5d50e91f3755c21f5843a7
Pratool <pratool.gadtaula@students.olin.edu>
2013-12-10 20:32:44 -0500

    Poster info

--- punchline.m
+++ punchline.m
@@ -1,4 +1,4 @@
-function plane()
+function punchline()
 % Modeling a Boeing 727
 
     g = 9.81;       % acceleration due to gravity (m/s^2)
@@ -8,45 +8,53 @@ function plane()
     C_l = 0.56;     % coefficient of lift (dimensionless)
     C_D = 0.6;      % coefficient of drag (dimensionless)
     
-    F_thrust = 1000000;
-    
+    F_thrust = 2000000;
+    F_thrust_2 = 2800000;
     initial_pos = [0, 0];
     initial_vel = [40, 10];
     
     options = odeset('events', @events);
     options2 = odeset('events', @events2);
     
-    [TIME, Y] = ode45(@thrust_on, [0, 2000], [initial_pos, initial_vel], options);
-    TIME1 = TIME;
-    t = TIME(end) - TIME(1);
-    [TIME, Y_2] = ode45(@thrust_off, [2000, 4000], [Y(end, 1), Y(end, 2), Y(end, 3), Y(end, 4)], options2);
-    
+    [TIME, Y] = ode45(@thrust_on_1, [0, 2000], [initial_pos, initial_vel], options);
+%     t_1 = TIME(1);
+%     t = TIME(end) - TIME(1);
+    [TIME_2, Y_2] = ode45(@thrust_off, [TIME(end), 4000], [Y(end, 1), Y(end, 2), Y(end, 3), Y(end, 4)], options2);
     Y = [Y; Y_2];
+    T = [TIME; TIME_2];
     
-    vector = thrust_on(TIME1(:, end), Y(TIME1(:, end-1)) );
-    acceleration = [ vector[2, end], vector[3, end] ];
-    g_force = norm(acceleration + [0; -9.8]);
+    [TIME_2, Y_2] = ode45(@thrust_on_2, [TIME(end), 4000], [Y(end, 1), Y(end, 2), Y(end, 3), Y(end, 4)], options2);
+    disp(TIME_2(1));
+    vector = [Y_2(1, 3); Y_2(1, 4)];
+    acceleration = norm(vector)/9.8;
+    disp(acceleration);
     
     %disp(Y);
     
 %     for i = 2:10
-%         [TIME, Y_2] = ode45(@thrust_on, [2000*i, 2000*(i+1)], [Y(end, 1), Y(end, 2), Y(end, 3), Y(end, 4)], options);
+%         [TIME, Y_2] = ode45(@thrust_on_2, [T(end), 2000*(i+1)], [Y(end, 1), Y(end, 2), Y(end, 3), Y(end, 4)], options);
 %         Y = [Y; Y_2];
-%         t = t + (TIME(end) - TIME(1));
-%         [TIME, Y_2] = ode45(@thrust_off, [2000*(i+1), 2000*(i+2)], [Y(end, 1), Y(end, 2), Y(end, 3), Y(end, 4)], options2);
+%         T = [T; TIME];
+% %         t = t + (TIME(end) - TIME(1));
+%         [TIME, Y_2] = ode45(@thrust_off, [T(end), 2000*(i+2)], [Y(end, 1), Y(end, 2), Y(end, 3), Y(end, 4)], options2);
 %         Y = [Y; Y_2];
+%         T = [T; TIME];
 %     end
 
+    clf;
     hold on;
-    disp(t);
-    plot(Y(:,1), Y(:,2), 'LineWidth', 2);
+    plot(T, Y(:,2), 'r', 'LineWidth', 2)
+    xlabel('Time (Seconds)', 'FontSize', 14);
+    %disp(t); %time spent thrusting
+    %disp(TIME(end) - t_1); %total time of flight
+    %plot(Y(:,1), Y(:,2), 'LineWidth', 2);
     %plot(Y_2(:,1), Y_2(:,2));
     %axis([0, 2e4, 0, 10000]);
-    xlabel('Horizontal Position (m)', 'FontSize', 14);
+    %xlabel('Horizontal Position (m)', 'FontSize', 14);
     ylabel('Vertical Position (m)', 'FontSize', 14);
     title('Trajectory of Zero-Gravity Aircraft', 'FontSize', 18);
 
-    function res = thrust_on(t, W)
+    function res = thrust_on_1(t, W)
         P = W(1:2);
         V = W(3:4);
         
@@ -61,6 +69,25 @@ function plane()
         
         dPdt = V;
         dVdt = [0; -g] + ([F_thrust*cos(pi/4);F_thrust*cos(pi/4)]/m) + ((rho*A*v^2) / (2*m)) * (C_l*s_hat - C_D*v_hat);
+        
+        res = [dPdt; dVdt];
+    end
+
+    function res = thrust_on_2(t, W)
+        P = W(1:2);
+        V = W(3:4);
+        
+        v = norm(V);
+        v_hat = V / v;
+        s_hat = fliplr(v_hat);
+        
+        %disp(t);
+        %disp(F_thrust);
+        %hold on;
+        %plot(t, F_thrust, 'o');
+        
+        dPdt = V;
+        dVdt = [0; -g] + ([F_thrust_2*cos(pi/4);F_thrust_2*cos(pi/4)]/m) + ((rho*A*v^2) / (2*m)) * (C_l*s_hat - C_D*v_hat);
         
         res = [dPdt; dVdt];
     end
@@ -85,14 +112,14 @@ function plane()
     end
 
     function [value, isterminal,direction] = events(t, W) % kill thrust when height is increasing
-        %value = W(2) - 9756;
-        value = W(2) - 9000;
+        value = W(2) - 8500;
+        %value = W(2) - 9000;
         direction = 1;
         isterminal = 1;
     end
 
     function [value, isterminal, direction] = events2(t, W) % when plane is too close to ground terminate
-        value = W(2) - 8000;
+        value = W(2) - 7300;
         
         direction = -1;
         isterminal = 1;
